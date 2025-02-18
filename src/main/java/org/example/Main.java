01ede6a5aaf7792aa22c0fef42b9dc46d9c5fe23
Alex <7zaytsev00@mail.ru>
2025-02-18 23:39:08 +0700

    Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,32 @@
-package org.example;public class Main {
+package org.example;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        args = new String[]{"-a", "-f", "-p", "tr", "t.txt", "t2.txt"};
+
+        try {
+            InputParser parser = new CLParser(args);
+            Info info = parser.parse();
+
+            FileReader fileReader = new FileReader(info.files());
+            Filter filter = new Filter(fileReader.read());
+            filter.filter();
+            FileWriter fileWriter = new FileWriter(filter, info.options());
+            fileWriter.write();
+
+
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println(illegalArgumentException.getMessage());
+        } catch (IOException exception) {
+            System.out.println("An error occurred while working with files");
+        } catch (Exception exception) {
+            System.out.println("Something went wrong");
+        }
+
     }
 }
